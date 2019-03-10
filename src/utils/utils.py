53d40090c39156e3d849630b870ca21f0d141b57
Elizeu Ribeiro Sanches Xavier <elizeu.xavier@gmail.com>
2019-03-10 10:26:31 -0300

    Put license in module utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,9 +1,24 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # vim:fileencoding=utf-8
 import os
 
 __license__ = 'GPL v3'
 __copyright__ = '2019, Elizeu Xavier <elizeu.xavier at gmail.comt>'
+
+# This file is part of Kinema.
+
+# Kinema is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Kinema is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Kinema.  If not, see <https://www.gnu.org/licenses/>.
 
 #
 # Name: ValidateDirectoryWritable Function
