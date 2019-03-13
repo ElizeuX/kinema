f7cb3d303b3fcc2d7cc7802568df820adc90cf7c
Elizeu Ribeiro Sanches Xavier <elizeu.xavier@gmail.com>
2019-03-13 06:56:24 -0300

    Include mainUI.py and splash.py

---
+++ src/view/mainUI.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# vim:fileencoding=utf-8
+
+__license__ = 'GPL v3'
+__copyright__ = '2019, Elizeu Xavier <elizeu.xavier at gmail.comt>'
+
+# This file is part of Kinema.
+
+# Kinema is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Kinema is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Kinema.  If not, see <https://www.gnu.org/licenses/>.
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from threading import Thread
+from time import sleep
+
+class MainUI(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+
+        # Set position and decoration
+        self.set_position(Gtk.WindowPosition.CENTER)
+        self.lbl = Gtk.Label()
+        self.lbl.set_label("Main window started")
+        self.add(self.lbl)
+        self.connect('destroy', Gtk.main_quit)
+
+        # Initiate and show the splash screen
+        print(("Starting splash"))
+        splash = Splash()
+        splash.start()
+
+        print(("Simulate MainUI work"))
+        sleep(5)
+
+        # Destroy splash
+        splash.destroy()
+        print(("Splash destroyed"))
+
+        print(("Starting MainUI"))
+        self.show_all()
---
+++ src/view/splash.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from threading import Thread
+
+
+class Splash(Thread):
+    def __init__(self):
+        super(Splash, self).__init__()
+
+        # Create a popup window
+        self.window = Gtk.Window(Gtk.WindowType.POPUP)
+        self.window.set_position(Gtk.WindowPosition.CENTER)
+        self.window.connect('destroy', Gtk.main_quit)
+        self.window.set_default_size(400, 250)
+
+        # Add box and label
+        box = Gtk.Box()
+        lbl = Gtk.Label()
+        lbl.set_label("My app is loading...")
+        box.pack_start(lbl, True, True, 0)
+        self.window.add(box)
+
+    def run(self):
+        # Show the splash screen without causing startup notification
+        # https://developer.gnome.org/gtk3/stable/GtkWindow.html#gtk-window-set-auto-startup-notification
+        self.window.set_auto_startup_notification(False)
+        self.window.show_all()
+        self.window.set_auto_startup_notification(True)
+
+        # Need to call Gtk.main to draw all widgets
+        Gtk.main()
+
+    def destroy(self):
+        self.window.destroy()
+
