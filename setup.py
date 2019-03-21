df514309fdbb6e7e234ff55cd582fa1ef311e6f0
Elizeu Ribeiro Sanches Xavier <ElizeuX@users.noreply.github.com>
2019-03-21 08:11:50 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,21 @@
 from setuptools import setup
 setup(
     name='kinema',
+    version='0.1', 
+    author='Elizeu Ribeiro Sanches Xavier',
+    author_email='elizeu.xavier@gmail.com',
     packages=['src', 'test'],
     test_suite='test',
+    classifiers=[
+          'Development Status :: 1 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: Portuguese',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ]
 )
